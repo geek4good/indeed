e2f9afd58ae6f4559b4a99a99ec761b98b8f4998
Lucas Mbiwe <lucas@geek4good.com>
2018-12-16 15:20:55 +0000

    Set up rough outside-in test
    Create a scaffold to test the indeed:jobs rake task by setting up a failing test
    that runs the command and captures the output along the exit status.

--- spec/indeed_spec.rb
+++ spec/indeed_spec.rb
@@ -1,9 +1,26 @@
+require "open3"
+
 RSpec.describe Indeed do
   it "has a version number" do
     expect(Indeed::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "rake indeed:jobs" do
+    it "outputs job listings in JSON format" do
+      search_term = "sales"
+      output, status = Open3.capture2("rake indeed:jobs", :stdin_data => search_term )
+
+      expect(status.exitstatus).to eq(0)
+      expect(output).to eq(<<~EOT)
+      [
+        {
+        "company":"some company",
+        "job_title":"job title",
+        "wage":"wage",
+        "description":"description"
+        }
+      ]
+      EOT
+    end
   end
 end
